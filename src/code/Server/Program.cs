5b27b81fbaa309792e8a33b7df1c5692b01c5776
Xorcist <Xorcist@Comcast.net>
2022-11-24 14:20:20 -0500

    Codebase added

---
+++ src/code/Server/Program.cs
@@ -0,0 +1,25 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Build web host
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+  app.UseWebAssemblyDebugging();
+} else {
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+app.MapFallbackToFile("index.html");
+
+// Run web host
+app.Run();
